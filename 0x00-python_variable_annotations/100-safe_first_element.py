6db3fa388a442f6065f1bd98a2a6a81585dc0709
Robson-Ali <aliye.r@yahoo.com>
2024-01-14 05:01:09 +0000

    Advanced tasks

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
 # The types of the elements of the input are not know
-from typing import Any, Union
+from typing import Any, Sequence, Union
 
-def safe_first_element(lst: Union[list, tuple]) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     if lst:
         return lst[0]
     else:
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,7 +1,16 @@
-from typing import List, Optional, TypeVar
+#!/usr/bin/env python3
+from typing import Any, Mapping, Union, TypeVar
 
-def safely_get_value(dct: dict, key: str, default: Optional[int] = None) -> Optional[int]:
+
+T = TypeVar('T')
+Res = Union[Any, T]
+Def = Union[T, None]
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
+    '''a function that gets the value from a
+    dictionary using the key'''
     if key in dct:
         return dct[key]
     else:
-        return default
+        return
