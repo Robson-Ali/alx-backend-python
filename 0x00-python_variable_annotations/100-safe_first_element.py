1e8db8547e30cf90b2b099f6da07dbbff5df40ca
Robson-Ali <aliye.r@yahoo.com>
2024-01-14 04:42:43 +0000

    Advanced task 1

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,6 +1,7 @@
-from typing import List, Optional
+# The types of the elements of the input are not know
+from typing import Any, Union
 
-def safe_first_element(lst: List[Optional[int]]) -> Optional[int]:
+def safe_first_element(lst: Union[list, tuple]) -> Union[Any, None]:
     if lst:
         return lst[0]
     else:
