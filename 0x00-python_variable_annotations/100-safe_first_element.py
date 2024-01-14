1634637fe976a89971fd2aa72d0db638358762f7
Robson-Ali <aliye.r@yahoo.com>
2024-01-14 05:04:20 +0000

    Advanced task

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
-# The types of the elements of the input are not know
+''' Duck typing - first element of a sequence
+'''
 from typing import Any, Sequence, Union
 
+
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''Augment the following code with the correct
+    duck-typed annotation
+    '''
     if lst:
         return lst[0]
     else:
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+'''Given the parameters and the return values, add type
+annotations to the function
+
+Hint: look into TypeVar.
+'''
 from typing import Any, Mapping, Union, TypeVar
 
 
